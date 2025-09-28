73690707ce03223433f2371582cd48d7579e2799
Joecy Neto <joecy.neto@gmail.com>
2025-09-28 07:05:15 -0300

    heroku arqs

--- caderno/settings.py
+++ caderno/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t-m$!&=#hm^-#ez!u505k71u0wt*(+iy&hfuc)q$##cc=9mg7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
